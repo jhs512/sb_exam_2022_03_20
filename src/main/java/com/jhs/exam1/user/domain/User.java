a3a6b239c97d43841842002cee70fa8d9efde515
jhs512 <jangka512@gmail.com>
2022-03-26 16:18:07 +0900

    롬ë복 적용

--- src/main/java/com/jhs/exam1/user/domain/User.java
+++ src/main/java/com/jhs/exam1/user/domain/User.java
@@ -1,5 +1,7 @@
 package com.jhs.exam1.user.domain;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,13 +9,14 @@ import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public long id;
-    public LocalDateTime regDate;
-    public LocalDateTime updateDate;
-    public String email;
-    public String password;
-    public String name;
+    private long id;
+    private LocalDateTime regDate;
+    private LocalDateTime updateDate;
+    private String email;
+    private String password;
+    private String name;
 }
